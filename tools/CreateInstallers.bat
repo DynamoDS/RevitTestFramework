e0e4238430e8e84c2191eaf7dc258c5789f15e33
Ian Keough <ian@iankeough.com>
2014-06-10 11:02:15 -0700

    Updates to installer script.

--- tools/CreateInstallers.bat
+++ tools/CreateInstallers.bat
@@ -1,3 +1,12 @@
 SET git=%USERPROFILE%\AppData\Local\GitHub\PORTAB~1\bin\git.exe
+SET msbuild=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe
+
+REM Checkout and build RTF for Revit 2014
 %git% checkout master
-MSBuild.exe ..\src\RevitTestFramework.sln /p:Configuration=Release
+%msbuild% ..\src\RevitTestFramework.sln /t:rebuild /p:platform="Any CPU" /p:Configuration=Release
+
+REM Checkout and build RTF for Revit 2015
+%git% checkout Revit2015
+%msbuild% ..\src\RevitTestFramework.sln /t:rebuild /p:platform="Any CPU" /p:Configuration=Release
+
+
