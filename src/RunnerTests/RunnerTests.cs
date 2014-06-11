fbaa8c83e9700cc2b91122fd785d63834ca1ca95
Ian Keough <ian@iankeough.com>
2014-06-11 10:26:05 -0700

    Add a test project.

---
+++ src/RunnerTests/RunnerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Runner;
+
+namespace RunnerTests
+{
+    [TestFixture]
+    public class RunnerTests
+    {
+        public void CleansUpJournalFilesWhenComplete()
+        {
+        }
+    }
+}
