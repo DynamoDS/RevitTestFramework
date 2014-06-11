624d0bd8404e941f0e492737c1430d60842dea2f
Ian Keough <ian@iankeough.com>
2014-06-11 12:20:04 -0700

    Start some tests for the runner.

--- src/Tests/RunnerTests/RunnerTests.cs
+++ src/Tests/RunnerTests/RunnerTests.cs
@@ -1,12 +1,35 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.Remoting;
+using System.Security.AccessControl;
+using Moq;
+using NUnit.Framework;
+using RTF.Framework;
 
 namespace RTF.Tests
 {
     [TestFixture]
     public class RunnerTests
     {
-        public void CleansUpJournalFilesWhenComplete()
+        [Test]
+        public void RunnerSetup()
         {
+            var runner = new Runner(){Gui=false};
+
+            Assert.IsNull(runner.WorkingDirectory);
+            Assert.IsNull(runner.TestAssembly);
+            Assert.IsNull(runner.RevitPath);
+            Assert.AreEqual(runner.SelectedProduct, 0);
+            Assert.IsFalse(runner.Gui);
+            Assert.IsFalse(runner.IsDebug);
+            Assert.AreEqual(runner.Assemblies.Count, 0);
+        }
+
+        [Test]
+        public void CanFindRevit()
+        {
+            var products = Runner.FindRevit();
+            Assert.GreaterOrEqual(products.Count, 1, "No Revit products could be found, or Revit is not installed on this machine.");
         }
     }
 }
