8ad29a2a68bacde8ead237dadf23b44b056805e5
Ian Keough <ian.keough@autodesk.com>
2015-02-20 13:26:50 -0800

    Add drag and drop.
    Drag and drop is now available in the assembly box, the working
    directory box, the results box, and the list view.

--- src/Applications/RevitTestFrameworkGUI/MainWindow.xaml.cs
+++ src/Applications/RevitTestFrameworkGUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -40,6 +41,47 @@ namespace RTF.Applications
         private void ProductSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             vm.ChangeProductCommand.Execute(((ComboBox) sender).SelectedIndex);
+        }
+
+        private void Assembly_OnDrop(object sender, DragEventArgs e)
+        {
+            vm.SetAssemblyPathCommand.Execute(GetFirstFileFromDropPackage(e));
+        }
+
+        private void WorkingDir_OnDrop(object sender, DragEventArgs e)
+        {
+            vm.SetWorkingPathCommand.Execute(GetFirstFileFromDropPackage(e));
+        }
+
+        private void OnPreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private string GetFirstFileFromDropPackage(DragEventArgs e)
+        {
+            //http://stackoverflow.com/questions/5662509/drag-and-drop-files-into-wpf
+            //http://stackoverflow.com/questions/4281857/wpf-drag-and-drop-to-a-textbox
+
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                return string.Empty;
+            }
+
+            // Note that you can have more than one file.
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (!files.Any())
+            {
+                return string.Empty;
+            }
+
+            return files.First();
+        }
+
+        private void Results_OnDrop(object sender, DragEventArgs e)
+        {
+            vm.SetResultsPathCommand.Execute(GetFirstFileFromDropPackage(e));
         }
     }
 }
