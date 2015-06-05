6e8f02583556702ff3dee3d1d461f1b336ab636e
Ian Keough <ian.keough@autodesk.com>
2015-06-05 11:11:07 -0700

    Output console to RTF view.

---
+++ src/Applications/RevitTestFrameworkGUI/TextBoxOutputter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Controls;
+
+namespace RTF.Applications
+{
+    public class TextBoxOutputter : TextWriter
+    {
+        TextBox textBox = null;
+
+        public TextBoxOutputter(TextBox output)
+        {
+            textBox = output;
+        }
+
+        public override void Write(char value)
+        {
+            base.Write(value);
+            textBox.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                textBox.AppendText(value.ToString());
+            }));
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+    }
+}
