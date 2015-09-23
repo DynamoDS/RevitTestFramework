aaeac8b491715e04ecd18ebbb17fec48b458dca3
Randy-Ma <zhaoyang.ma@autodesk.com>
2015-09-23 14:40:35 +0800

    Take address string as a const data

---
+++ src/Framework/RevitTestFramework/CommonData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RTF.Framework
+{
+    public class CommonData
+    {
+        const string strLocalIPAddress = "127.0.0.1";
+        public static string LocalIPAddress
+        {
+            get
+            {
+                return strLocalIPAddress;
+            }
+        }
+    }
+}
