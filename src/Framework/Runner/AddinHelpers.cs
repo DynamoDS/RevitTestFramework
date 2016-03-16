14a30e0386c34451d1028c8c3feae9869de68ef1
Michael Kirschner <mjk.kirschner@gmail.com>
2016-03-16 19:12:05 -0400

    add addin helper class with simple method to parse XML and overwrite addin file
    use the helper when copying addins

---
+++ src/Framework/Runner/AddinHelpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Linq;
+
+namespace RTF.Framework
+{
+    static class AddinHelpers
+    {
+        /// <summary>
+        /// this method modifies Addin files so that their assembly paths
+        /// are fully qualified
+        /// </summary>
+        /// <param name="addinFile"></param>
+        /// <returns></returns>
+        public static void FullyQualifyAddinPaths (FileInfo addinFile)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(addinFile.FullName);
+
+            foreach(XmlElement addinElement in doc.DocumentElement.ChildNodes)
+            {
+                //if this element is an addin attempt to make the assembly path a full path
+             if (addinElement.LocalName != "Addin")
+                {
+                    continue; 
+                }
+                var assemblies = addinElement.ChildNodes.OfType<XmlElement>().Where(x => x.LocalName == "Assembly");
+                var paths = assemblies.Select(x => x.InnerText);
+                var files = paths.Select(x => new FileInfo(x));
+                var fullpaths = files.Select(x => x.FullName);
+
+                assemblies.ToList().Zip(fullpaths, (asm, path) => asm.InnerText = path).ToList();
+            }
+            doc.Save(addinFile.FullName);
+        }
+    }
+}
